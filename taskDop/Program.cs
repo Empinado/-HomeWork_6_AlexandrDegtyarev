40875facf5241007c3048763ef08406fdcefece7
Alexandr Degtyarev <Sasha20111997@gmail.com>
2022-12-02 23:18:09 +0500

    added taskDop

---
+++ taskDop/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу для подсчета количества цифр в числе с помощью рекурсии.
+// 12345 -> 5
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count(int n)
+{
+    if (n / 10 != 0)
+    {
+        return 1 + count(n / 10);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int countNum = count(number);
+
+Console.WriteLine(countNum);
